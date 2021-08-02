2b44fed5b176b6d8d9d6912a32f6727d5737269d
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-08-02 16:32:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+index.js: 1 Hello Winc Academy
+index.html: 39 Live reload enabled.
+let name = 'Nouhayla';
+undefined
+console.log("Nouhayla");
+VM188: 1 Nouhayla
+undefined
+let name = 'if';
+undefined
+4 + 4
+8
+console.log(8);
+VM255: 1 8
+undefined
+"4" + "4"
+"44"
+console.log(44);
+VM311: 1 44
+undefined
+8 - 5
+3
+console.log(3);
+VM367: 1 3
+undefined
+8 * 3
+24
+console.log(24);
+VM410: 1 24
+undefined
+56 / 7
+8
+console.log(8);
+VM453: 1 8
+undefined
+2 % 8
+2
+console.log(2);
+VM491: 1 2
+undefined
+let age = '100';
+undefined
+console.log(typeof '100');
+VM655: 1 string
+undefined
+console.log(typeof 100);
+VM680: 1 number
+undefined
+let text = 'one hundred';
+undefined
+console.log(typeof "one hundred");
+VM821: 1 string
+undefined
