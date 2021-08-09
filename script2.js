cffd7edd8373863195f391840a4d0c26379babe9
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-08-09 11:46:32 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+Hello Winc Academy
+index.html: 39 Live reload enabled.
+// Dit is een grote som.
+let multiplication = 1230941 * 1823794;
+undefined
+console.log("multiplication:", multiplication);
+VM279: 1 multiplication: 2244982810154
+undefined
+let division = 637263 / 54;
+undefined
+console.log("division", division);
+VM509: 1 division 11801.166666666666
+undefined
+/* Dit is een blok comment
+om  de 2 voorgaande rekensommen in 1 comment te zetten */
