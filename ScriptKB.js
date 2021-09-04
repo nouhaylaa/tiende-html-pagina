de54f80bdcc92f03a657066da57149e7105a0217
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-09-04 16:45:54 +0200

    Add files via upload

---
+++ ScriptKB.js
@@ -0,0 +1,25 @@
+const age = "number";
+var myAge = 18;
+if (myAge >= 18) {
+    document.write("Je mag naar binnen")
+}
+else {
+    document.write("Je mag niet naar binnen")
+
+}
+const isFemale = "boolean";
+var genderisFemale = true;
+if (genderisFemale = false) {
+    document.write("Je mag niet naar binnen")
+}
+else {
+    document.write(", Ladies Night")
+}
+const driverStatus = "bob";
+var driverIs = bob;
+if (driverIs = bob) {
+    document.write("Je bent Bob!")
+}
+else {
+    document.write("Je bent geen Bob")
+}
