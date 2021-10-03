fffa98efc9a7a6c5ae041f1fc0dd33db30a991b5
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-10-03 15:09:41 +0200

    Add files via upload

---
+++ ScriptKB2.js
@@ -0,0 +1,25 @@
+const age = 18;
+const firstName = "Sarah";
+const totalAmount = 100;
+
+if (age >= 18) {
+    console.log("Je mag naar binnen!");
+}
+else {
+    console.log("Je mag niet naar binnen");
+}
+if (age >= 18 && age <= 25) {
+    console.log("Je krijgt 50% korting!");
+}
+if (firstName === "Sarah" || firstName === "Bram") {
+    console.log("Gefeliciteerd! Je krijgt een gratis drankje");
+}
+if (totalAmount >= 25 && totalAmount < 50) {
+    console.log("Je krijgt gratis (vega)bitterballen!");
+}
+if (totalAmount >= 50 && totalAmount < 100) {
+    console.log("Je krijgt gratis een portie nachos!");
+}
+if (totalAmount >= 100) {
+    console.log("Je krijgt gratis een fles sinaasappelsap!");
+}
