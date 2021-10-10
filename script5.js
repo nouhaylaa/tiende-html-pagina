286646cbda5e8f032d8aa6e40cdd8cbd4075b526
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-10-10 18:25:36 +0200

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,19 @@
+const makePizza = function () {
+    console.log("Roll the dough");
+    console.log("Add sauce and topping");
+    console.log("Put the pizza in the oven");
+    console.log("Take the pizza out the oven and serve it");
+};
+const makeSushi = function () {
+    console.log("Cook the sushi rice");
+    console.log("Chop the vegetables and fish");
+    console.log("Drain the rice");
+    console.log("Spread the rice on the sushi mat");
+    console.log("Add the vegetables and fish");
+    console.log("Roll the sushi and cut it");
+};
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
