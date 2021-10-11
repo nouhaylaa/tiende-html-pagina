df13ee2ec9c9d81810dd366f6fb36869b2474273
nouhaylaa <87525212+nouhaylaa@users.noreply.github.com>
2021-10-11 20:01:01 +0200

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,15 @@
+// Function declaration
+function squareNumber(number) {
+    console.log(number * number);
+};
+squareNumber(5);
+// Function expression
+const squareNumber2 = function (number) {
+    console.log(number * number);
+};
+squareNumber2(4);
+//  Arrow function
+const squareNumber3 = (number) => {
+    console.log(number * number);
+};
+squareNumber3(3);
